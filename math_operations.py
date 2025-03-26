096d8d1170d7f423d146e12f0b6ac2a627d47d6b
NandiniPD <nandinidodawad8@gmail.com>
2025-03-26 12:34:54 +0530

    Initial commit with math operations and unit tests

---
+++ math_operations.py
@@ -0,0 +1,11 @@
+"""
+This module provides basic math operations.
+"""
+
+def add(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+def multiply(a, b):
+    """Returns the product of two numbers."""
+    return a * b
